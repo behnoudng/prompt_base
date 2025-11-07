902fae837c32e922ab1df63b02f63c419f4dbfe9
behnoudng <behnoudng@gmail.com>
2025-11-07 18:55:26 +0000

    add sign up and profile urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
-
+from .views import SignUpView, ProfileView
 urlpatterns = [
+    path("signup/", SignUpView.as_view(), name='signup'),
+    path('profile/', ProfileView.as_view(), name='profile')
 ]
