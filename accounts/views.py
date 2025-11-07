42a73c96d1839f120df5e14411917873e1c84620
behnoudng <behnoudng@gmail.com>
2025-11-07 18:05:52 +0000

    views for login and sign up

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.views.generic import TemplateView 
-
-
+from .forms import SignUpForm, ProfileUpdateForm
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import User
+class SignUpView(CreateView):
+    model = User
+    form_class = SignUpForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy('home')
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileUpdateForm
+    template_name = 'profile.html'
+    success_url = reverse_lazy('profile')
+    def get_object(self):
+        return self.request.user
+    
--- prompt_base/urls.py
+++ prompt_base/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('accounts/', include('accounts.urls')),
     path('', include('core.urls'))
 ]
