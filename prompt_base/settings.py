17a50abce923edda421771e7f40f8ae18bf2187b
Ghazal <ghazallbonakdar@gmail.com>
2025-12-14 21:10:14 +0800

    add sidebar

--- prompt_base/settings.py
+++ prompt_base/settings.py
@@ -69,7 +69,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static',
+                    'media']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
